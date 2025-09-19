7a1aedd964807099e117c057e4a077fa91f14db0
CLARA <tucorreo@ejemplo.com>
2025-09-19 19:07:08 -0300

    Se agrega otra trasicion entre rutas

--- src/transicionRutas.ts
+++ src/transicionRutas.ts
@@ -1,8 +1,7 @@
 import { trigger, transition, style, animate, query, group } from '@angular/animations';
 
-
 export const transicionRutas = trigger('transicionRutas', [
-  transition('* => *', [
+  transition('home => *, proyectos => sobreMi', [
     style({ position: 'relative', overflow: 'hidden' }),
 
 
@@ -42,5 +41,45 @@ export const transicionRutas = trigger('transicionRutas', [
         }))
       ], { optional: true })
     ])
+  ]), 
+  transition('sobreMi => *, proyectos => home', [   
+  style({ position: 'relative', overflow: 'hidden' }),
+
+  query(':enter', [
+    style({ 
+      position: 'absolute',
+      top: '6vh',
+      left: 0,
+      width: '100%',
+      height: '96%',
+      transform: 'translateY(-100%)', 
+      opacity: 1
+    })
+  ], { optional: true }),
+
+  group([
+    // El que sale
+    query(':leave', [
+      style({ 
+        position: 'absolute',
+        top: '6vh',
+        left: 0,
+        width: '100%',
+        height: '96%',
+        transform: 'translateY(0)',
+        opacity: 1
+      }),
+      animate('0.4s ease-out', style({ 
+        transform: 'translateY(100%)'   
+      }))
+    ], { optional: true }),
+
+    // El que entra
+    query(':enter', [
+      animate('0.4s ease-out', style({ 
+        transform: 'translateY(0)'
+      }))
+    ], { optional: true })
   ])
+])
 ]);
